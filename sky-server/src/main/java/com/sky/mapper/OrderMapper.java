02981d02ce3802313559f6237ee28f89381a0417
yangmingzhe <yang13930079212@gmail.com>
2026-05-03 21:46:58 +0200

    订单状态定时处理功能相关代码

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -6,6 +6,9 @@ import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Mapper
 public interface OrderMapper {
     void insert(Orders orders);
@@ -19,4 +22,7 @@ public interface OrderMapper {
     Integer countStatus(Integer toBeConfirmed);
 
     void update(Orders orders);
+
+    @Select("select * from orders where status = #{status} and order_time = #{orderTime}")
+    List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime orderTime);
 }
